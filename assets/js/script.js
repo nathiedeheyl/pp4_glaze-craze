48d7051ccd6d7b0f1549dc53e21fefb8e48fc209
Nathalie von <nath.vonheyl@gmail.com>
2024-12-17 17:14:14 +0000

    feat: add transition effect to responsive toggle down menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,15 @@
 const toggleBtn = document.querySelector('.toggle-btn')
-const toggleBtnIcon = document.querySelector('.toggle-btn i')
 const dropDownMenu = document.querySelector('.navbar')
 
 // DROPDOWN MENU
-
-toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('on')
-}
+toggleBtn.addEventListener("click", () => {
+    if (dropDownMenu.classList.contains("off")) {
+        // enable toggle down menu
+        dropDownMenu.classList.remove("off")
+        dropDownMenu.classList.add("on");
+    } else {
+        // disable transition for toggle down menu
+        dropDownMenu.classList.remove("on")
+        dropDownMenu.classList.add("off");
+    };
+});
